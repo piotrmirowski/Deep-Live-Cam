b158b32b9a43cbc7aec2edbd94db6232f22e7c01
Piotr Mirowski <piotrmirowski@Piotrs-MacBook-Pro.local>
2025-04-22 10:47:02 +0200

    Clean up code

--- run_deep_fake.py
+++ run_deep_fake.py
@@ -6,40 +6,16 @@ from flask_cors import CORS, cross_origin
 import io
 import time
 import threading
-from typing import List
 
 import cv2
-import onnxruntime
-from PIL import Image, ImageOps
-import platform
-import resource
-import tensorflow
+import numpy as np
+from PIL import Image
 
 from modules import core
 from modules.face_analyser import get_one_face
 import modules.globals
 from modules.processors.frame.core import get_frame_processors_modules
 
-
-def suggest_max_memory() -> int:
-  if platform.system().lower() == 'darwin':
-    return 4
-  return 16
-
-
-def encode_execution_providers(execution_providers: List[str]) -> List[str]:
-    return [execution_provider.replace('ExecutionProvider', '').lower()
-            for execution_provider in execution_providers]
-
-
-def suggest_execution_providers() -> List[str]:
-  return encode_execution_providers(onnxruntime.get_available_providers())
-
-
-def decode_execution_providers(execution_providers: List[str]) -> List[str]:
-  return [provider for provider, encoded_execution_provider
-          in zip(onnxruntime.get_available_providers(), encode_execution_providers(onnxruntime.get_available_providers()))
-          if any(execution_provider in encoded_execution_provider for execution_provider in execution_providers)]
 
 
 parser = argparse.ArgumentParser(description='Deep Fake server')
@@ -53,29 +29,10 @@ parser.add_argument('--width', help='width in pixels', dest='width',
 parser.add_argument('--height', help='height in pixels', dest='height',
                     type=int, default=540)
 parser.add_argument('--max-memory', help='maximum amount of RAM in GB', dest='max_memory',
-                    type=int, default=suggest_max_memory())
+                    type=int, default=core.suggest_max_memory())
 parser.add_argument('--execution-provider', help='execution provider', dest='execution_provider',
-                    default=['coreml'], choices=suggest_execution_providers(), nargs='+')
+                    default=['coreml'], choices=core.suggest_execution_providers(), nargs='+')
 args = parser.parse_args()
-
-
-def limit_resources() -> None:
-  # prevent tensorflow memory leak
-  gpus = tensorflow.config.experimental.list_physical_devices('GPU')
-  for gpu in gpus:
-    tensorflow.config.experimental.set_memory_growth(gpu, True)
-  # limit memory usage
-  if modules.globals.max_memory:
-    memory = modules.globals.max_memory * 1024 ** 3
-    if platform.system().lower() == 'darwin':
-      memory = modules.globals.max_memory * 1024 ** 6
-    if platform.system().lower() == 'windows':
-      import ctypes
-      kernel32 = ctypes.windll.kernel32
-      kernel32.SetProcessWorkingSetSize(-1, ctypes.c_size_t(memory), ctypes.c_size_t(memory))
-    else:
-      import resource
-      resource.setrlimit(resource.RLIMIT_DATA, (memory, memory))
 
 
 def log(msg: str, msg_type: str) -> None:
@@ -97,7 +54,7 @@ def setup() -> None:
   modules.globals.video_encoder = "libx264"
   modules.globals.video_quality = 18
   modules.globals.max_memory = args.max_memory
-  modules.globals.execution_providers = decode_execution_providers(args.execution_provider)
+  modules.globals.execution_providers = core.decode_execution_providers(args.execution_provider)
   modules.globals.execution_threads = 8
   modules.globals.fp_ui['face_enhancer'] = False
   modules.globals.nsfw = False
@@ -107,24 +64,22 @@ def setup() -> None:
     if not frame_processor.pre_check():
       log("Could not pre-check frame_processor", "error")
       return
-  limit_resources()
+  core.limit_resources()
 
 
 source_image = {"image": None, "annotated_image": None, "timestamp": 0}
-current_camera = {"image": None, "timestamp": 0}
-current_deepfake = {"image": None, "timestamp": 0, "active": True}
-latest_source_streamed_frame = {"timestamp": 0}
-latest_deepfake_streamed_frame = {"timestamp": 0}
+current_camera = {"image": None, "byte_string": None, "timestamp": 0}
+current_deepfake = {"image": None, "byte_string":None, "timestamp": 0, "active": True}
 
 
 def copy_from_alt_temp_file() -> None:
   """Capture the source image from the camera."""
-  print("copy_from_alt_temp_file")
   log(f"Copying alt image...", "source")
   cv2_image = cv2.imread("images/deep1.jpg")
-  print("capture_source_image_from_camera", type(cv2_image), cv2_image.shape, cv2_image.max())
+  log(f"Image of type {type(cv2_image)}, shape {cv2_image.shape}, max {cv2_image.max()}", "source")
   source_image["image"] = cv2_image
   source_image["annotated_image"] = get_one_face(cv2_image)
+  source_image["byte_string"] = write_numpy_to_byte_string(source_image["image"])
   source_image["timestamp"] = time.time()
   log(f"Temporary image saved to {args.camera_image_path}...", "source")
   cv2.imwrite(args.camera_image_path, cv2_image)
@@ -132,13 +87,13 @@ def copy_from_alt_temp_file() -> None:
 
 def capture_source_image_from_camera() -> None:
   """Capture the source image from the camera."""
-  print("capture_source_image_from_camera")
   if current_camera["image"] is not None:
     log(f"Capturing camera image...", "source")
     cv2_image = current_camera["image"].copy()
-    print("capture_source_image_from_camera", type(cv2_image), cv2_image.shape, cv2_image.max())
+    log(f"Image of type {type(cv2_image)}, shape {cv2_image.shape}, max {cv2_image.max()}", "source")
     source_image["image"] = cv2_image
     source_image["annotated_image"] = get_one_face(cv2_image)
+    source_image["byte_string"] = write_numpy_to_byte_string(source_image["image"])
     source_image["timestamp"] = time.time()
     log(f"Temporary image saved to {args.camera_image_path}...", "source")
     cv2.imwrite(args.camera_image_path, cv2_image)
@@ -149,10 +104,33 @@ def load_source_image_from_file() -> None:
   if modules.globals.source_path:
     log(f"Loading image {modules.globals.source_path}...", "source")
     cv2_image = cv2.imread(modules.globals.source_path)
-    print("load_source_image_from_file", type(cv2_image), cv2_image.shape, cv2_image.max())
+    log(f"Image of type {type(cv2_image)}, shape {cv2_image.shape}, max {cv2_image.max()}", "source")
     source_image["image"] = cv2_image
     source_image["annotated_image"] = get_one_face(cv2_image)
+    source_image["byte_string"] = write_numpy_to_byte_string(source_image["image"])
     source_image["timestamp"] = time.time()
+
+
+def write_numpy_to_byte_string(image: np.ndarray):
+  """Write numpy array image onto a bytestream."""
+  if image is not None:
+    frame = io.BytesIO()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    image.save(frame, format='JPEG')
+    return frame.getvalue()
+  else:
+    return None
+
+
+def write_image_to_byte_string(image: Image.Image):
+  """Write PIL image onto a bytestream."""
+  if image is not None:
+    frame = io.BytesIO()
+    image.save(frame, format='JPEG')
+    return frame.getvalue()
+  else:
+    return None
 
 
 def run_deep_fake_loop() -> None:
@@ -184,21 +162,20 @@ def run_deep_fake_loop() -> None:
     # Create a copy of the camera frame and store it.
     current_camera["image"] = camera_frame.copy()
     current_camera["timestamp"] = time.time()
+    current_camera["byte_string"] = write_numpy_to_byte_string(current_camera["image"])
 
     # Process the camera frame to create the deep fake.
-    temp_frame = camera_frame.copy()
+    fake_image = camera_frame.copy()
     if current_deepfake["active"] is True:
       try:
         for frame_processor in frame_processors:
-          temp_frame = frame_processor.process_frame(source_image["annotated_image"], temp_frame)
+          fake_image = frame_processor.process_frame(source_image["annotated_image"], fake_image)
       except:
-        print("NEED TO TAKE NEW PICTURE")
+        log("NEED TO TAKE NEW PICTURE", "error")
 
     # Convert the image to RGB format to display it with Tkinter and store it.
-    fake_image = cv2.cvtColor(temp_frame, cv2.COLOR_BGR2RGB)
-    fake_image = Image.fromarray(fake_image)
-    current_deepfake["image"] = ImageOps.contain(
-        fake_image, (PREVIEW_MAX_WIDTH, PREVIEW_MAX_HEIGHT), Image.LANCZOS)
+    current_deepfake["image"] = fake_image
+    current_deepfake["byte_string"] = write_numpy_to_byte_string(current_deepfake["image"])
     current_deepfake["timestamp"] = time.time()
 
 
@@ -209,51 +186,43 @@ thread.start()
 def source_stream():
   """Loop that streams the most recent image source."""
 
-  last_source_frame = None
+  latest_byte_string = None
+  latest_timestamp = 0
   while True:
-    if latest_source_streamed_frame["timestamp"] < source_image["timestamp"]:
-      print(f'[source_stream] stream: {latest_source_streamed_frame["timestamp"]}')
-      image = source_image["image"]
-      latest_source_streamed_frame["timestamp"] = source_image["timestamp"]
-      if image is not None:
-        frame = io.BytesIO()
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(image)
-        image.save(frame, format='JPEG')
-        last_source_frame = frame.getvalue()
+    if latest_timestamp < source_image["timestamp"]:
+      log(f"stream: {latest_timestamp}", "source_stream")
+      latest_timestamp = source_image["timestamp"]
+      latest_byte_string = source_image["byte_string"]
+      if latest_byte_string is not None:
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + last_source_frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + latest_byte_string + b'\r\n')
     else:
-      if last_source_frame:
+      if latest_byte_string:
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + last_source_frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + latest_byte_string + b'\r\n')
       else:
-        time.sleep(0.1)
+        time.sleep(0.001)
 
 
 def deepfake_stream():
   """Loop that streams the camera / deep fake image using the last input / result."""
 
-  last_frame = None
+  latest_byte_string = None
+  latest_timestamp = 0
   while True:
-    if latest_deepfake_streamed_frame["timestamp"] < current_deepfake["timestamp"]:
-      print(f'[deepfake_stream] stream: {latest_deepfake_streamed_frame["timestamp"]}')
-      image = current_deepfake["image"]
-      latest_deepfake_streamed_frame["timestamp"] = current_deepfake["timestamp"]
-      if image is not None:
-        frame = io.BytesIO()
-        if image.size[0] != args.width or image.size[1] != args.height:
-          image = image.resize((args.width, args.height))
-        image.save(frame, format='JPEG')
-        last_frame = frame.getvalue()
+    if latest_timestamp < current_deepfake["timestamp"]:
+      log(f"stream: {latest_timestamp}", "deepfake_stream")
+      latest_timestamp = current_deepfake["timestamp"]
+      latest_byte_string = current_deepfake["byte_string"]
+      if latest_byte_string is not None:
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + last_frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + latest_byte_string + b'\r\n')
     else:
-      if last_frame:
+      if latest_byte_string:
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + last_frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + latest_byte_string + b'\r\n')
       else:
-        time.sleep(0.01)
+        time.sleep(0.001)
 
 
 # Define the app, and wrap it in CORS handler.
