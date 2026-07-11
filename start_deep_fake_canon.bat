016214e44d9cc70ea5541f8c8b15e9b8eecc9643
Piotr Mirowski <piotr.mirowski@computer.org>
2026-07-11 23:53:26 +0100

    Add batch files

--- start_deep_fake_canon.bat
+++ start_deep_fake_canon.bat
@@ -1,3 +1,4 @@
 ECHO off
-ECHO Run deep fake using laptop webcam
-venv\Scripts\python.exe run_deep_fake.py --width 960 --height 540 --execution-provider cuda --device "EOS Webcam Utility"
+ECHO Run deep fake using Canon EOS Camera
+call ..\gemini_api_key.bat
+call venv\Scripts\python.exe run_deep_fake.py --width 960 --height 540 --execution-provider cuda --device "EOS Webcam Utility"
--- start_deep_fake_webcam.bat
+++ start_deep_fake_webcam.bat
@@ -1,3 +1,4 @@
 ECHO off
 ECHO Run deep fake using Logitech webcam
-venv\Scripts\python.exe run_deep_fake.py --width 960 --height 540 --execution-provider cuda --device "Logitech"
+call ..\gemini_api_key.bat
+call venv\Scripts\python.exe run_deep_fake.py --width 960 --height 540 --execution-provider cuda --device "Logitech"
